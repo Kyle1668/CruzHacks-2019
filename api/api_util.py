5ca526854c56870ae370eebab13c4ee6d8d3077c
Kyle1668 <kyledevinobrien1@gmail.com>
2018-05-12 23:56:27 -0700

    Fixed lookup issues

--- api/api_util.py
+++ api/api_util.py
@@ -15,8 +15,7 @@ def generate_hacker_id(hackers_collection):
     records = hackers_collection.find()
 
     if records.count() > 0:
-        highest_id = records.sort("id", DESCENDING)[0]["id"]
-        print("highest_id: " + str(highest_id))
-        return str(int(highest_id) + 1)
+        highest_id = int(records.sort("id", DESCENDING)[0]["id"])
+        return str(highest_id + 1)
     else:
         return "1"
